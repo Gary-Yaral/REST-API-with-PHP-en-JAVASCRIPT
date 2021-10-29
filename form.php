e885a2b9de3d581527f89d58404f738f3aa72408
Gary Yaral <gary.yaral@gmail.com>
2021-10-29 12:23:03 -0500

    Falta arreglar la ventana de modificar

--- form.php
+++ form.php
@@ -8,36 +8,61 @@
   <title>API</title>
 </head>
 <body>
-  <h2>API Rest</h2>
-  <form id="form-add">
-    <section>
-      <div id="book-data" class="book-data">
-        <label for="name">Nombre:</label>
-        <input type="text" id="name" name="name">
-        <label for="description">Descripción:</label>
-        <input type="text" id="description" name="description">
-        <label for="author">Autor:</label>
-        <input type="text" id="author" name="author">
-        <label for="year">Año:</label>
-        <input type="text" id="year" name="year">
-      </div>
-      <input type="submit" value="Agregar">
+  <div class="container">
+    <h2>Books Rest API</h2>
+    <section class="data-form card">
+      <form id="form-add">
+        <section>
+          <div id="book-data" class="book-data">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name">
+            <label for="description">Description:</label>
+            <input type="text" id="description" name="description">
+            <label for="author">Author:</label>
+            <input type="text" id="author" name="author">
+            <label for="year">Year:</label>
+            <input type="text" id="year" name="year">
+          </div>
+          <input type="submit" value="Agregar">
+        </section>
+      </form>
     </section>
-  </form>
-  <table id="data-api" class="hidden">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Nombre</th>
-        <th>Descripción</th>
-        <th>Autor</th>
-        <th>Año</th>
-        <th>Opciones</th>
-      </tr>
-    </thead>
-    <tbody> 
-    </tbody>
-  </table>
+    <section class="data-table card">
+      <table id="data-api" class="hidden">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Autor</th>
+            <th>Año</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody> 
+        </tbody>
+      </table>
+    </section>
+  </div>
+  <footer>Powered by Gary Yaral &copy</footer>
+  <section id="modal" class="update hidden">
+    <div id="closeModal" class="close">X</div>
+    <form id="form-update" class="form-update">
+      <section>
+        <div id="book-data" class="book-data">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name">
+          <label for="description">Description:</label>
+          <input type="text" id="description" name="description">
+          <label for="author">Author:</label>
+          <input type="text" id="author" name="author">
+          <label for="year">Year:</label>
+          <input type="text" id="year" name="year">
+        </div>
+        <input type="submit" value="Agregar">
+      </section>
+    </form>
+  </section>
   <script src="js/api.js"></script>
 </body>
 </html>
--- js/api.js
+++ js/api.js
@@ -3,6 +3,9 @@ let response = document.querySelector('.json-response');
 let method = document. querySelector('#method');
 let data= document.querySelector('#json');
 let table = document.querySelector('table');
+let closeModal = document.querySelector('#closeModal');
+let updateModal = document.querySelector('#modal');
+let updateForm = document.querySelector('#form-update');
 
 function renderTable(data) {
   let table = document.querySelector('table');
@@ -11,14 +14,16 @@ function renderTable(data) {
   data.forEach(book => {
     let tr = document.createElement('tr');
     tr.innerHTML = `
-      <td>${book.id}</td>
+      <td class='table-id'>${book.id}</td>
       <td>${book.name}</td>
       <td>${book.description}</td>
       <td>${book.author}</td>
       <td>${book.year}</td>
       <td>
-        <button class='edit'>Editar</button>
-        <button class='delete'>Eliminar</button>
+        <div>
+          <button class='edit'>Editar</button>
+          <button class='delete'>Eliminar</button>
+        </div>
       </td>
     `;
     tbody.append(tr);
@@ -40,22 +45,43 @@ get_data();
 table.addEventListener('click', (e) => {
   let btn = e.target;
   if (btn.classList.contains('delete')) {
-    let parent = btn.parentNode.parentNode;  
+    let parent = btn.parentNode.parentNode.parentNode;  
     delete_data(parent);
+  }
+
+  if (btn.classList.contains('edit')) { 
+    updateModal.classList.remove('hidden'); 
+    let parent = btn.parentNode.parentNode.parentNode;
+    let id = parent.querySelectorAll('td')[0].innerHTML;
+    updateModal.setAttribute('update',id);
+
   }
 })
 
-function update(data, response) {
+function update(idUpdate, form) {
+  let formData = new FormData(form);
+  let name = formData.get('name');
+  let description = formData.get('description');
+  let author = formData.get('author');
+  let year = formData.get('year');
+  let data = {
+    "id":idUpdate,
+    "name": name,
+    "description": description,
+    "author": author,
+    "year": year
+  }
+  
   fetch('update.php',{
     method: 'PUT',
-    body: data.value,
+    body: JSON.stringify(data),
     headers:{
       "Content-type": "application/json;charset=UTF-8",
     },
   })
   .then(response => response.json())
-  .then(response_message =>  {
-    response.innerHTML = JSON.stringify(response_message);
+  .then(response =>  {
+    alert(response.message);
   })
 }
 
@@ -85,8 +111,9 @@ function insert() {
   get_data();
 }
 
-function delete_data(parent) {
-  let id = parent.querySelectorAll('td')[0].innerHTML;  
+function delete_data(parentNode) {
+  let id = parentNode.querySelectorAll('td')[0].innerHTML;
+
   fetch('delete.php',{
     method: 'DELETE',
     body: JSON.stringify({"id":parseInt(id)}),
@@ -96,14 +123,64 @@ function delete_data(parent) {
   })
     .then(response => response.json())
     .then(response =>  {
+      get_data();
       alert(response.message);
     })
-
-    get_data();
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  insert();
+function verifyEmpty(form) {
+  let data =new FormData(form);
+  let name = data.get('name');
+  let description = data.get('description');
+  let author = data.get('author');
+  let year = data.get('year');
+
+  if(name === '') {
+    form.querySelector('#name').focus();
+    return false;
+  }
+  if(description === '') {
+    form.querySelector('#description').focus();
+    return false;
+  }
+  if(author === '') {
+    form.querySelector('#author').focus();
+    return false;
+  }
+  if(year === '') {
+    form.querySelector('#year').focus();
+    return false;
+  }
+  return true;
+}
+
+closeModal.addEventListener('click', () => {
+  updateModal.classList.add('hidden');
+  console.log(updateModal)
+})
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (verifyEmpty(event.currentTarget) === false) {
+    alert('Debe llenar todos los campos');
+  }
+
+  if (verifyEmpty(formData) === true) {
+    insert();
+  }
+})
+
+updateForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (verifyEmpty(event.currentTarget) === false) {
+    alert('Debe llenar todos los campos');
+  }
+
+  if (verifyEmpty(event.currentTarget) === true) {
+    let id = updateModal.getAttribute('update');
+    update(id,event.currentTarget);
+  }
 })
 
